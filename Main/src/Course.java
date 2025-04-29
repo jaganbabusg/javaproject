da6a8503ffbcd1126df5889370be96742f1e13d8
Jagan Babu <sj_babu@yahoo.com>
2025-04-29 16:40:18 +0800

    javaproject

---
+++ Main/src/Course.java
@@ -0,0 +1,73 @@
+public class Course extends Student {
+    protected String CourseID;
+    protected String CourseName;
+    protected String CourseStatus; // Completed, In Progress, Not Started
+    protected int StudentAttendance; // 0-100%
+    protected boolean StudentCompletion; // (if course is completed and attendance is more than 75% then passed)
+
+    public Course() {
+        // Default constructor
+    }
+
+    public Course(String studentName, String studentID, int studentMobile, String studentEmail, String courseID,
+            String courseName, String courseStatus, int studentAttendance, boolean studentCompletion) {
+        super(studentName, studentID, studentMobile, studentEmail);
+        setCourseID(courseID);
+        setCourseName(courseName);
+        setCourseStatus(courseStatus);
+        setStudentAttendance(studentAttendance);
+        setStudentCompletion(studentCompletion);
+    }
+
+    public String getCourseID() {
+        return CourseID;
+    }
+
+    public void setCourseID(String courseID) {
+        CourseID = courseID;
+    }
+
+    public String getCourseName() {
+        return CourseName;
+    }
+
+    public void setCourseName(String courseName) {
+        CourseName = courseName;
+    }
+
+    public String getCourseStatus() {
+        return CourseStatus;
+    }
+
+    public void setCourseStatus(String courseStatus) {
+        CourseStatus = courseStatus;
+    }
+
+    public int getStudentAttendance() {
+        return StudentAttendance;
+    }
+
+    public void setStudentAttendance(int studentAttendance) {
+        StudentAttendance = studentAttendance;
+    }
+
+    public boolean isStudentCompletion() {
+        return StudentCompletion;
+    }
+
+    public void setStudentCompletion(boolean studentCompletion) {
+        StudentCompletion = studentCompletion;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Course-ID = " + getCourseID() + ", Course-Name = " + getCourseName() + ", Course-Status = "
+                + getCourseStatus() + ", Student-Attendance = " + getStudentAttendance() + ", Student-Completion = "
+                + (isStudentCompletion() ? "YES" : "NO");
+    }
+
+    public void displayCourse() {
+        System.out.println(super.toString() + ", Course-ID = " + getCourseID() + ", Course-Name = " + getCourseName() + ", Course-Status = "
+        + getCourseStatus() + ", Student-Attendance = " + getStudentAttendance() + ", Student-Completion = " + (isStudentCompletion() ? "YES" : "NO"));
+    }
+}
---
+++ Main/src/Student.java
@@ -0,0 +1,61 @@
+public class Student {
+
+    protected String StudentName;
+    protected String StudentID;
+    protected int StudentMobile;
+    protected String StudentEmail;
+
+    public Student() {
+        // Default constructor
+    }
+
+    public Student(String studentName, String studentID, int studentMobile, String studentEmail) {
+        setStudentName(studentName);
+        setStudentID(studentID);
+        setStudentMobile(studentMobile);
+        setStudentEmail(studentEmail);
+    }
+
+    public String getStudentName() {
+        return StudentName;
+    }
+
+    public void setStudentName(String studentName) {
+        StudentName = studentName;
+    }
+
+    public String getStudentID() {
+        return StudentID;
+    }
+
+    public void setStudentID(String studentID) {
+        StudentID = studentID;
+    }
+
+    public int getStudentMobile() {
+        return StudentMobile;
+    }
+
+    public void setStudentMobile(int studentMobile) {
+        StudentMobile = studentMobile;
+    }
+
+    public String getStudentEmail() {
+        return StudentEmail;
+    }
+
+    public void setStudentEmail(String studentEmail) {
+        StudentEmail = studentEmail;
+    }
+
+    @Override
+    public String toString() {
+        return "Student-Name = " + getStudentName() + ", Student-ID = " + getStudentID() + ", Student-Mobile = " + getStudentMobile()
+                + ", Student-Email = " + getStudentEmail();
+    }
+
+    public void displayStudent() {
+        System.out.println("Student-Name = " + getStudentName() + ", Student-ID = " + getStudentID() + ", Student-Mobile = " + getStudentMobile()
+                + ", Student-Email = " + getStudentEmail());
+    }
+}
